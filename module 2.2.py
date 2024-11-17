f11f015e48a71bca290dddd0d6ae0bd70757ae01
ZakharKurganov <kurgan4ik.19@gmail.com>
2024-11-17 21:06:02 +0500

    Add files via upload

---
+++ module 2.2.py
@@ -0,0 +1,14 @@
+first = int(input('введите первое число: '))
+second = int(input('введите второе число: '))
+third = int(input('введите второе число: '))
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
+
+
+
+
+
